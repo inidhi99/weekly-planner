42f7ad4ad87cf4ad84312b2c887e3107bd74ff9c
nidhi <inidhi99@gmail.com>
2022-10-06 19:07:33 -0400

    added timeblock in HTML and edited few CSS styling based on my preference

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// date and time using moment js
 var date = moment();
-$("#currentDay").text(date.format('MMMM Do YYYY, h:mm:ss a'));
+$("#currentDay").text(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
 
+// 
 
