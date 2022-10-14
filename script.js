06930a62e05bade204bd3548861eb51d89c935e7
nidhi <inidhi99@gmail.com>
2022-10-13 21:58:20 -0400

    added function for the color block, added local storage to save the data

--- script.js
+++ script.js
@@ -1,7 +1,61 @@
 // date and time using moment js
-var date = moment();
+
+function displayTime (){
+   var date = moment();
 $("#currentDay").text(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+}
+
+var displayTimeInterval = setInterval(displayTime, 1000);
 
 
+function time (){
+    var currentTime = moment().hours();
+    $(".description").each(function(){
+        var blockHour = parseInt($(this).parent().attr("id"))
+        if (blockHour< currentTime){
+            $(this).addClass("past")
+        } else if (blockHour === currentTime){
+            $(this).removeClass("past")
+            $(this).addClass("present")
+        } else {
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future")}
+    })
+}
+
+time();
+
+var interval = setInterval(time, 60000);
+
+
+
+$(".saveBtn").on("click", function(){
+    var hour = $(this).parent().attr("id");
+    var value = $(this).siblings(".description").val()
+    localStorage.setItem(hour, value);
+
+
+})
+
+
+$(document).ready(function(){
+    $("#9 .description").val(localStorage.getItem("9"))
+    $("#10 .description").val(localStorage.getItem("10"))
+    $("#11 .description").val(localStorage.getItem("11"))
+    $("#12 .description").val(localStorage.getItem("12"))
+    $("#13 .description").val(localStorage.getItem("13"))
+    $("#14 .description").val(localStorage.getItem("14"))
+    $("#15 .description").val(localStorage.getItem("15"))
+    $("#16 .description").val(localStorage.getItem("16"))
+    $("#17 .description").val(localStorage.getItem("17"))
+    $("#18 .description").val(localStorage.getItem("18"))
+    $("#19 .description").val(localStorage.getItem("19"))
+    $("#20 .description").val(localStorage.getItem("20"))
+    $("#21 .description").val(localStorage.getItem("21"))
+    $("#22 .description").val(localStorage.getItem("22"))
+    $("#23 .description").val(localStorage.getItem("23"))
+    $("#24 .description").val(localStorage.getItem("24"))
+})
 
 
